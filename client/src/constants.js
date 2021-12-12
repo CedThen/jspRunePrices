b9cec0453fdf8f8d37669f03b1c8c145c9f096e3
Ted <changxin.t.chen@gmail.com>
2021-12-12 18:47:32 -0500

    another url change ahhh

--- client/src/constants.js
+++ client/src/constants.js
@@ -2,6 +2,6 @@ const fixedUrl = `jspscraper.herokuapp.com`
 const url = process.env.ADDRESS_URL || 'localhost'
 console.log(`url`, url)
 
-export const fetchLatestApi = `http://${fixedUrl}/api/latest/`
-export const fetchLastAmountApi = (num) => `http://${fixedUrl}/api/last/${num}`
+export const fetchLatestApi = `https://${fixedUrl}/api/latest/`
+export const fetchLastAmountApi = (num) => `https://${fixedUrl}/api/last/${num}`
 
