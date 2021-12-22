6807da549a365b3430b31ca2de720a3696252730
Ted <changxin.t.chen@gmail.com>
2021-12-21 23:56:01 -0500

    new build

--- client/src/themes/index.js
+++ client/src/themes/index.js
@@ -1,14 +1,14 @@
 import { extendTheme } from '@chakra-ui/react'
-import { createBreakpoints } from '@chakra-ui/theme-tools'
+// import { createBreakpoints } from '@chakra-ui/theme-tools'
 
 // This is the default breakpoint
-const breakpoints = createBreakpoints({
-  sm: '30em',
-  md: '48em',
-  lg: '62em',
-  xl: '80em',
-  '2xl': '96em',
-})
+// const breakpoints = createBreakpoints({
+//   sm: '30em',
+//   md: '48em',
+//   lg: '62em',
+//   xl: '80em',
+//   '2xl': '96em',
+// })
 
 
 const theme = extendTheme({
